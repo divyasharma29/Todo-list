0302bb5202553d63371f20416a100ac72bffaa66
unknown <dvsharma.0720@gmail.com>
2020-09-27 10:19:40 +0530

    add .gitignore, procfile and update port

--- app.js
+++ app.js
@@ -138,8 +138,14 @@ app.get("/:itemListName", function(req, res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
 
-app.listen(3000 , function(){
+
+app.listen(port , function(){
     console.log("Server is running at 3000");
 });
 
